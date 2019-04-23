4d1f7f2f6cf63b0d858c32a081c5be007b38c5cf
lovdeepm <lovdeep@gmail.com>
2019-04-22 23:18:16 -0700

    DB settings

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,10 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "mander8385",
-  database: "burgers_db"
+  host: "ctgplw90pifdso61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "k0hys0t6586u3mjn",
+  password: "papms6wlnhlojsnw",
+  database: "l505csvsj7e9awhp"
 })
 }
 

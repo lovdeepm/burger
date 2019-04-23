5e09d52365bb29068e34bd389dc3ba97ac1cd64a
lovdeepm <lovdeep@gmail.com>
2019-04-22 22:54:15 -0700

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
+var connection; 
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
   user: "root",
   password: "mander8385",
   database: "burgers_db"
-});
+})
+}
 
 // Make connection.
 connection.connect(function(err) {
